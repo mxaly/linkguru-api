bb84facc1d8ede086f9cec5f6a73f5d47126677f
Rafał Gawlik <rafal.maly@gmail.com>
2014-05-25 20:50:59 +0200

    seeds: users, links, votes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,36 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'faker'
+
+puts 'Creating users...'
+10.times do
+  @created = false
+  while !@created do
+    @created = User.create(name: Faker::Name.name, email: Faker::Internet.free_email).save
+  end
+end
+
+puts 'Creating links...'
+150.times do
+  @created = false
+  while !@created do
+    user = User.all[rand(10)]
+    tags = Faker::Lorem.words(rand(4)+1)
+    @created = Link.create(
+      user: user,
+      description: Faker::Lorem.sentence,
+      tags_array: tags,
+      description: Faker::Lorem.sentence,
+      url: "https://www.google.com/#q=#{Faker::Lorem.word}"
+    ).save
+  end
+end
+
+puts 'Creating votes...'
+300.times do
+  @created = false
+  while !@created do
+    @user = User.all[rand(10)]
+    voteable_links = Link.all - @user.votes.map{ |v| v.voteable }
+    @link = voteable_links[rand(voteable_links.count)]
+    @created = Vote.create(user: @user, voteable: @link, value: ((rand(2) == 1) ? 1 : -1)).save
+  end
+end
